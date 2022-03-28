540f2d0bb8a37978e49e267181c3eaae6ac6562f
enizimus <eniz.m@outlook.com>
2022-03-28 11:09:30 +0200

    feat: fix import errors

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from distutils.core import setup
 
 
@@ -13,7 +12,7 @@ setup(
     url="https://github.com/enizimus/surmod",
     download_url="https://github.com/enizimus/surmod/archive/v0.1.2-beta.tar.gz",
     keywords=["surogate", "models", "sampling plan", "screening"],
-    install_requires=["numpy", "scipy", "matplotlib", "ypstruct", "seaborn"],
+    install_requires=["numpy", "scipy", "matplotlib", "ypstruct", "seaborn", "torch", "pymoo"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
@@ -22,28 +21,3 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
 )
-=======
-from distutils.core import setup
-
-
-setup(
-    name="surmod",
-    packages=["surmod"],
-    version="0.1.1",
-    license="MIT",
-    description="Surrogate modelling library for python including code for variable screening, sampling plans and models",
-    author="Eniz Museljic",
-    author_email="eniz.m@outlook.com",
-    url="https://github.com/enizimus/surmod",
-    download_url="https://github.com/enizimus/surmod/archive/v0.1.1-beta.tar.gz",
-    keywords=["surogate", "models", "sampling plan", "screening"],
-    install_requires=["numpy", "scipy", "matplotlib", "ypstruct"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
-    ],
-)
->>>>>>> dev
